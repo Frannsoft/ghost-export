ab91327f126d6d4f20973fb04a573050acf512aa
Frannsoft <Frannsoftdev@outlook.com>
2019-06-22 14:58:06 -0400

    added support for v2 Ghost JSON export files

--- bin/cli.js
+++ bin/cli.js
@@ -1,43 +1,55 @@
 #!/usr/bin/env node
 
-var GhostExport = require('..'),
-    package = require('../package.json'),
-    program = require('commander')
-      .version(package.version)
-      .usage('[options...] [source] [destination]')
-      .option('-d --drafts', 'Export drafts only')
-      .option('-a --all', 'Export both published posts and drafts');
+const GhostExport = require('../lib/ghost-export'),
+  JsonGhostExport = require('../lib/json-ghost-export'),
+  package = require('../package.json'),
+  program = require('commander')
+    .version(package.version)
+    .usage('[options...] [source] [destination]')
+    .option('-j --jsonexport', 'Export file is a JSON file from Ghost v2+')
+    .option('-d --drafts', 'Export drafts only')
+    .option('-a --all', 'Export both published posts and drafts');
 
-program.on('--help', function(){
+program.on('--help', function() {
   console.log('  Description:');
   console.log('');
   console.log('    ' + package.description);
   console.log('');
   console.log('  Examples:');
   console.log('');
-  console.log('    # Export published posts only')
+  console.log('    # Export published posts only');
   console.log('    $ ghost-export /path/to/ghost/app /path/to/output');
   console.log('');
-  console.log('    # Export drafts only')
+  console.log('    # Export drafts only');
   console.log('    $ ghost-export --drafts /path/to/ghost/app /path/to/output');
   console.log('');
-  console.log('    # Export all posts')
+  console.log('    # Export all posts');
   console.log('    $ ghost-export --all /path/to/ghost/app /path/to/output');
   console.log('');
 });
 
 program.parse(process.argv);
-
 var args = {
   source: program.args.shift(),
   destination: program.args.shift(),
-  published: (!program.drafts || program.all) ? true : false,
-  drafts: (program.drafts || program.all)
+  published: !program.drafts || program.all ? true : false,
+  drafts: program.drafts || program.all,
+  sourceType: program.jsonexport ? 'json' : 'sqlite'
 };
+if (args.sourceType === 'sqlite') {
+  console.log('test');
 
-GhostExport(args, function(err, count) {
-  if (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-});
+  GhostExport(args, function(err, count) {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  });
+} else if (args.sourceType === 'json') {
+  JsonGhostExport(args, function(err, count) {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  });
+}
